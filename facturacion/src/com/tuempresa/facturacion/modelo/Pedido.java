a59a39af9ac3382fa3a306911459351f4efcef04
LEYDEN2 <114091510+LEYDEN2@users.noreply.github.com>
2023-02-10 02:52:05 -0500

    comit1

---
+++ facturacion/src/com/tuempresa/facturacion/modelo/Pedido.java
@@ -0,0 +1,79 @@
+package com.tuempresa.facturacion.modelo;
+
+import java.time.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+
+import org.openxava.annotations.*;
+import org.openxava.util.*;
+
+import lombok.*;
+
+@Entity @Getter @Setter
+@View(extendsView="super.DEFAULT", 
+members=
+    "diasEntregaEstimados, entregado," +  
+    "factura { factura }"
+)
+@View(name = "SinClienteNiFactura",
+       members = 
+       "anyo, numero, fecha;"
+       + "detalles;"
+       + "observaciones"
+		)
+
+
+public class Pedido extends DocumentoComercial{
+
+	@ManyToOne
+	@ReferenceView("SinClienteNiPedidos")
+	Factura factura;
+ 
+	@Depends("fecha")
+	public int getDiasEntregaEstimados() {
+	    if (getFecha().getDayOfYear() < 15) {
+	        return 20 - getFecha().getDayOfYear(); 
+	    }
+	    if (getFecha().getDayOfWeek() == DayOfWeek.SUNDAY) return 2;
+	    if (getFecha().getDayOfWeek() == DayOfWeek.SATURDAY) return 3;
+	    return 1;
+	}
+	@Column(columnDefinition="INTEGER DEFAULT 1")
+	int diasEntrega;
+	
+	@PrePersist @PreUpdate 
+	private void recalcularDiasEntrega() {
+	    setDiasEntrega(getDiasEntregaEstimados());
+	}
+	
+	@Column(columnDefinition = "BOOLEAN DEFAULT FALSE")
+	boolean entregado;
+
+	@AssertTrue(message="pedido_debe_estar_entregado" )
+		private boolean isEntregadoParaEstarEnFactura() { 
+			return factura == null || isEntregado(); 	
+			} 
+	
+	@PreRemove
+	private void validarPreBorrar() {
+	if (factura != null) {  
+		throw new javax.validation.ValidationException( // Lanza una excepción runtime
+	XavaResources.getString( 
+"no_puede_borrar_pedido_con_factura"));
+	}
+	}
+	
+	//@PrePersist @PreUpdate  
+	//private void validar() throws Exception {
+		//if (factura != null && !isEntregado()) {  
+			//throw new javax.validation.ValidationException(
+				//	XavaResources.getString(  
+					//		"pedido_debe_estar_entregado",
+						//	getAnyo(),
+							//getNumero())
+					//);
+	//	}
+	//}
+}
+
